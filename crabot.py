ae12dc03840c8c53c1bf611e6ac7b4ed18057546
Pavel Vokhmyanin <pavel.vokhmyanin@gmail.com>
2016-11-17 03:23:38 +0300

    Pre-Alpha commit
    Added:
    1) collecting stats, increasing currency each 10 messages
    2) reading&saving stats to file

--- crabot.py
+++ crabot.py
@@ -2,30 +2,81 @@
 from ws4py.client.threadedclient import WebSocketClient
 import json
 import requests
+import ast
+import pprint
+
+#import username, password and channel_id from config_crabot.py
 from config_crabot import *
 
-class DummyClient(WebSocketClient):
+pp = pprint.PrettyPrinter(indent=4)
+
+class ggClient(WebSocketClient):
+    us = open("userstat.txt", "r")
+    try:
+        user_stat = ast.literal_eval(us.read())
+    finally:
+        us.close()
     def opened(self):
-        self.send(json.dumps({"type":"auth", "data":{"user_id":"0"}}))
+        print "Connecting to channel %d..." % channel_id
+
+        join_message = json.dumps({"type":"join","data":{"channel_id":channel_id,"hidden":False}})
+        get_userlist = json.dumps({"type":"get_users_list2","data":{"channel_id":channel_id}})
+        leave_message = json.dumps({"type":"unjoin","data":{"channel_id":channel_id}})
+
+        if username and password:
+            r = requests.post("https://goodgame.ru/ajax/chatlogin", data={'login': username, 'password': password})
+            jresponse = json.loads(r.text)
+        if username and password and jresponse["result"]:
+            print "Authentication Success"
+            print "Welcome back, captain %s!" % username
+            user_id = jresponse["user_id"]
+            token = jresponse["token"]
+            auth_message = json.dumps({"type":"auth", "data":{"user_id" : user_id, "token" : token }})
+        else:
+            print "Authentication Failed: %s" % json
+            print "Will use guest user..."
+            auth_message = json.dumps({"type":"auth", "data":{"user_id":"0"}})
+        self.send(auth_message)
+        self.send(join_message)
     def closed(self, code, reason=None):
-        print "Closed down", code, reason
+        us = open("userstat.txt", "w")
+        try:
+            us.write(str(self.user_stat))
+        finally:
+            us.close()        
     def received_message(self, m):
-        print json.dumps(json.loads(m.data.decode("utf-8")), indent=4, sort_keys=True)
-
-r = requests.post("https://goodgame.ru/ajax/chatlogin", data={'login': login, 'password': password})
-auth_response = r.text
-
-auth_message = json.dumps({"type":"auth", "data":{"user_id":"0"}})
-join_message = json.dumps({"type":"join","data":{"channel_id":44451,"hidden":False}})
-get_userlist = json.dumps({"type":"get_users_list","data":{"channel_id":44451}})
-leave_message = json.dumps({"type":"unjoin","data":{"channel_id":44451}})
-
+        user_stat = self.user_stat
+        recv_json = json.loads(m.data.decode("utf-8"))
+        if recv_json["type"] == "message":
+            user = recv_json["data"]["user_name"]
+            if user_stat.has_key(user):
+                user_stat[user]["messages"] += 1
+                if user_stat[user]["messages"] % 10 == 0:
+                    user_stat[user]["money"] += 1
+            else:
+                user_stat[user] = {}
+                user_stat[user]["messages"] = 1
+                user_stat[user]["money"] = 0
+            print "%s[#%d,$%d]: %s" % (user, user_stat[user]["messages"], user_stat[user]["money"], recv_json["data"]["parsed"])
+        else:
+            if recv_json["type"] != "channel_counters":
+                print json.dumps(recv_json)
 try:
-    ws = DummyClient('ws://chat.goodgame.ru:8081/chat/websocket', protocols = ['http-only', 'chat'])
+    ws = ggClient('ws://chat.goodgame.ru:8081/chat/websocket', protocols = ['http-only', 'chat'])
     ws.connect()
-    ws.send(join_message)
-    ws.send(get_userlist)
     ws.run_forever()
 except KeyboardInterrupt:
     ws.close()
+    us = open("userstat.txt", "w")
+    try:
+        us.write(str(ws.user_stat))
+    finally:
+        us.close()
+    print ""
+    pp.pprint(ws.user_stat)
 
+'''
+
+>>> {"type":"users_list","data":{"channel_id":44451,"clients_in_channel":"4","users_in_channel":2,"users":[{"id":"457651","name":"NafanyaShow","rights":20,"premium":false,"premiums":[],"banned":false,"baninfo":false,"payments":"0","hidden":false,"stuff":"0"},{"id":"647206","name":"destroer1990","rights":10,"premium":true,"premiums":[0],"banned":false,"baninfo":false,"payments":"6","hidden":false,"stuff":"0"}]}}
+
+'''
